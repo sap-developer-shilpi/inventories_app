02befc2ccf79ebb15982391be69957e5759e86aa
Shilpi sharma <sap.developer.shilpi@gmail.com>
2017-10-11 11:56:01 +0000

    show page styled and gravatar added for representative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(location, options = { size: 100})
+   gravatar_id = Digest::MD5::hexdigest(location.email.downcase)
+   size = options[:size]
+   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+   image_tag(gravatar_url, alt: location.primary_representative, class: "img-circle")
+  end
 end
